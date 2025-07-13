7d186aca56080666a8e6ca13a2e0882f9cff1c4e
jiwanne <juckyjoo225@naver.com>
2025-07-13 18:08:45 +0900

    아직 고쳐야하는데.. controller 랑 mapper , jsp 쪽 손봣어요 그메인 형식으로 다시 받아서 전부 고쳐야함 servlet도 이상한거 들어가져잇어서 dispacher가 없어져버림 어제 깨지면서 다시 받고 일단 합치는거 내일 제가 할게요

---
+++ Dasi_Bom/src/main/java/service/WitnessService.java
@@ -0,0 +1,36 @@
+package service;
+
+import java.util.List;
+
+import config.DBManager;
+import dto.WitnessDTO;
+import mapper.WitnessMapper;
+
+public class WitnessService {
+	
+	private static WitnessService instance = new  WitnessService();
+	private WitnessMapper mapper;
+	
+	private WitnessService() {
+		mapper = DBManager.getInstance().getSession().getMapper(WitnessMapper.class);
+	}
+	
+	public static WitnessService getInstance() {
+		if(instance == null)
+			instance = new WitnessService();
+		return instance;
+	}
+	
+	public List<WitnessDTO> selectWitnessList() {
+		return mapper.selectWitnessList();
+	}
+	
+	public boolean insertWitness(WitnessDTO dto) {
+	    return mapper.insertWitness(dto) > 0;
+	}
+
+	public WitnessDTO selectWitnessById(String id) {
+		 return mapper.selectWitnessById(id);
+	}
+
+}
