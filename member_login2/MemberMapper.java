098e3af3fe960aae76c32a7adced915f06ff3d01
dlgurals100 <dlgurals100@naver.com>
2025-07-16 16:41:11 +0900

    재경아 내가했어

---
+++ member_login2/MemberMapper.java
@@ -0,0 +1,16 @@
+package mapper;
+
+import java.util.Map;
+import dto.MemberDTO;
+
+public interface MemberMapper {
+
+    
+    MemberDTO memberLogin(Map<String, Object> map);
+
+    
+    Integer selectById(String memberId);
+
+    
+    int insertMember(MemberDTO dto);
+}
